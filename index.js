1feab950b31a8277213226dc2f3f672059e608a1
Brendan Rius <brendan.rius@gmail.com>
2016-09-22 09:51:55 +0200

    Ready :)

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ module.exports = {
   contentFor: function (type, config) {
     if (type === 'body-footer') {
 
-      if (!config.crisp || !config.crisp.WRITE_KEY) {
+      if (!config.crisp || !config.crisp.WEBSITE_ID) {
         return '';
       }
 
       return '<script type="text/javascript">CRISP_WEBSITE_ID = "' + config.crisp.WEBSITE_ID + '";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.im/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);window.crisp})();</script>';
     }
-  }
+  },
 };
